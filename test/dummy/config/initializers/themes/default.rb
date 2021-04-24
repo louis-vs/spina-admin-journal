940ebbaf9b82f1465be55dfcb6fb5d634849e7b9
Louis Van Steene <louisvansteene@gmail.com>
2021-04-24 11:46:56 +0100

    Update config

--- test/dummy/config/initializers/themes/default.rb
+++ test/dummy/config/initializers/themes/default.rb
@@ -4,7 +4,7 @@
   theme.name = 'default'
   theme.title = 'Default Theme'
 
-  theme.page_parts = [{
+  theme.parts = [{
     name: 'text',
     title: 'Text',
     partable_type: 'Spina::Text'
@@ -13,13 +13,13 @@
   theme.view_templates = [{
     name: 'homepage',
     title: 'Homepage',
-    page_parts: ['text']
+    parts: ['text']
   }, {
     name: 'show',
     title: 'Default',
     description: 'A simple page',
     usage: 'Use for your content',
-    page_parts: ['text']
+    parts: ['text']
   }]
 
   theme.custom_pages = [{
