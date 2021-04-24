d523a25366adb11709f1c034574bd46026b1e584
Louis Van Steene <louisvansteene@gmail.com>
2021-04-24 14:11:26 +0100

    Set locale before all actions

--- app/controllers/spina/admin/journal/application_controller.rb
+++ app/controllers/spina/admin/journal/application_controller.rb
@@ -9,10 +9,16 @@ module Spina
 
         layout :admin_layout, only: %i[new edit]
 
+        before_action :set_locale
+
         private
 
         def admin_layout
           'spina/admin/admin'
+        end
+
+        def set_locale
+          @locale = params[:locale] || I18n.default_locale
         end
       end
     end
