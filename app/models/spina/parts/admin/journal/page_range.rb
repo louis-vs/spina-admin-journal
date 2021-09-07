08e7094990f68e6aa9e78ccb6ad82207da18d06b
Louis Van Steene <louis@lvs.me.uk>
2021-09-07 12:24:30 +0100

    Add validations and tests for page range part

--- app/models/spina/parts/admin/journal/page_range.rb
+++ app/models/spina/parts/admin/journal/page_range.rb
@@ -9,8 +9,20 @@ module Spina
           attr_json :first_page, :integer
           attr_json :last_page, :integer
 
+          validate :valid_range
+
           def content
             self
+          end
+
+          def valid_range
+            return if first_page.nil? && last_page.nil?
+
+            if last_page.nil?
+              errors.add :last_page, 'must be present'
+            elsif last_page < first_page
+              errors.add :last_page, 'must be larger than the first page'
+            end
           end
         end
       end
--- test/models/spina/parts/admin/journal/page_range_test.rb
+++ test/models/spina/parts/admin/journal/page_range_test.rb
@@ -7,9 +7,32 @@ module Spina
     module Admin
       module Journal
         class PageRangeTest < ActiveSupport::TestCase
-          # test "the truth" do
-          #   assert true
-          # end
+          setup do
+            @new_page_range = PageRange.new
+          end
+
+          test 'should save valid page range' do
+            @new_page_range.first_page = 1
+            @new_page_range.last_page = 2
+            assert @new_page_range.valid?
+          end
+
+          test 'should save empty page range' do
+            assert @new_page_range.valid?
+          end
+
+          test 'should not save page range with empty last page' do
+            assert @new_page_range.valid?
+            @new_page_range.first_page = 1
+            assert @new_page_range.invalid?
+          end
+
+          test 'should not save page range with smaller last page' do
+            assert @new_page_range.valid?
+            @new_page_range.first_page = 2
+            @new_page_range.last_page = 1
+            assert @new_page_range.invalid?
+          end
         end
       end
     end
