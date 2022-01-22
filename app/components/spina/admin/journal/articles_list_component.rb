45e4e2ee34f2dfb9ae778eec185a51394cd4c9ca
Louis Van Steene <louis@lvs.me.uk>
2022-01-22 13:21:52 +0000

    Redesign and refactor articles views

---
+++ app/components/spina/admin/journal/articles_list_component.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Spina
+  module Admin
+    module Journal
+      class ArticlesListComponent < ListComponent
+        def initialize(articles:)
+          @articles = articles
+        end
+
+        def before_render
+          @list_items = generate_list_items(@articles)
+        end
+
+        def call
+          render ListComponent.new(list_items: @list_items, sortable: false)
+        end
+
+        private
+
+        def generate_list_items(articles)
+          articles.map do |article|
+            { id: article.id,
+              label: generate_label(article),
+              path: helpers.spina.edit_admin_journal_article_path(article.id) }
+          end
+        end
+
+        def generate_label(article)
+          t('spina.admin.journal.articles.article_number', volume_number: article.issue.volume.number,
+                                                           issue_number: article.issue.number,
+                                                           article_number: article.number) + ' | ' + t('spina.admin.journal.articles.title_author', title: article.title, author: article.affiliations.map { |a| a.surname }.join(', '))
+        end
+      end
+    end
+  end
+end
