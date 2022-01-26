274df8e8b7440674381ab66acbb2ba51c80ea93d
Louis Van Steene <louis@lvs.me.uk>
2022-01-26 11:48:46 +0000

    Expose template files to tailwind

--- lib/spina/admin/journal/engine.rb
+++ lib/spina/admin/journal/engine.rb
@@ -7,6 +7,17 @@ module Spina
       class Engine < ::Rails::Engine
         isolate_namespace Spina::Admin::Journal
 
+        config.to_prepare do
+          Spina.configure do |config|
+            config.tailwind_content << "#{Spina::Admin::Journal::Engine.root}/app/views/**/*.*"
+            config.tailwind_content << "#{Spina::Admin::Journal::Engine.root}/app/components/**/*.*"
+            config.tailwind_content << "#{Spina::Admin::Journal::Engine.root}/app/helpers/**/*.*"
+            config.tailwind_content << "#{Spina::Admin::Journal::Engine.root}/app/assets/javascripts/**/*.js"
+            config.tailwind_content << "#{Spina::Admin::Journal::Engine.root}/app/**/application.tailwind.css"
+          end
+          puts Spina.config.tailwind_content
+        end
+
         config.before_initialize do
           ::Spina::Plugin.register do |plugin|
             plugin.name = 'journal'
