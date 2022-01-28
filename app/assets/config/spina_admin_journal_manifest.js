64e0a57aa56d2c85acd0596c750df51b5f15078f
Louis Van Steene <louis@lvs.me.uk>
2022-01-28 11:29:58 +0000

    Fix manifest
    It turns out .keep files are not packaged with the gem, leading to
    sprockets errors if you load the manifest. Since they weren't being used
    anyway, I've just removed the unused folders and references to them.

--- app/assets/config/spina_admin_journal_manifest.js
+++ app/assets/config/spina_admin_journal_manifest.js
@@ -1,6 +1,2 @@
 //= link spina/admin/journal/application.css
-
-//= link_directory ../javascripts/spina/admin/journal/controllers
-//= link_directory ../javascripts/spina/admin/journal/libraries
-
 //= link spina/admin/journal/application.js
